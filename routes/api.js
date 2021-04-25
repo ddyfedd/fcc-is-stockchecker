fc2f181fca94a67a0ce35890563afc28d87a8e02
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-25 12:09:41 +0200

    api is almost set up, one functional test complete

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,88 @@
 'use strict';
 
+const expect = require('chai').expect;
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+
 module.exports = function (app) {
+
+  let uri = process.env.DB;
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+  let stockSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    likes: {type: Number, default: 0},
+    ips: [String]
+  });
+
+  let Stock = mongoose.model('Stock', stockSchema);
 
   app.route('/api/stock-prices')
     .get(function (req, res){
+      let responseObject = {};
+      responseObject['stockData'] = {};
+
+      let twoStocks = false;
+
+      let outputResponse = () => {
+        return res.json(responseObject);
+      };
+
+      let findOrUpdateStock = (stockName, documentUpdate, nextStep) => {
+        Stock.findOneAndUpdate(
+          {name: stockName},
+          documentUpdate,
+          {new: true, upsert: true},
+          (err, stockDocument) => {
+            if(err) {
+              console.log(err);
+            } else if(!err && stockDocument) {
+              if(twoStocks === false) {
+                return nextStep(stockDocument, processOneStock);
+              }
+            }
+          });
+      };
+
+      let likeStock = (stockName, nextStep) => {
+
+      };
+
+      let getPrice = (stockDocument, nextStep) => {
+        let xhr = new XMLHttpRequest();
+        let requestUrl = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/' + stockDocument['name'] + '/quote';
+        xhr.open('GET', requestUrl, true);
+        xhr.onload = () => {
+          let apiResponse = JSON.parse(xhr.responseText);
+          stockDocument['price'] = parseFloat(apiResponse['latestPrice'].toFixed(2));
+          nextStep(stockDocument, outputResponse);
+        };
+        xhr.send();
+      };
+
+      let processOneStock = (stockDocument, nextStep) => {
+        responseObject['stockData']['stock'] = stockDocument['name'];
+        responseObject['stockData']['price'] = stockDocument['price'];
+        responseObject['stockData']['likes'] = stockDocument['likes'];
+        nextStep();
+      };
+
+      let stocks = [];
+      let processTwoStock = (stockDocument, nextStep) => {
+
+      };
+
+      if (typeof (req.query.stock) === 'string') {
+        let stockName = req.query.stock;
+
+        let documentUpdate = {};
+        findOrUpdateStock(stockName, documentUpdate, getPrice);
+
+      } else if (Array.isArray(req.query.stock)) {
+
+      }
       
     });
     
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,19 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
+  suite('GET /api/stock-prices => stockData object', () => {
+    test('1 stock', (done) => {
+      chai.request(server).get('/api/stock-prices')
+        .query({ stock: 'goog' })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.body['stockData']['stock'], 'goog');
+          assert.isNotNull(res.body['stockData']['price']);
+          assert.isNotNull(res.body['stockData']['likes']);
+          done();
+        });
+    });
 
+    
+  });
 });
